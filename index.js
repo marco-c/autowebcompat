334cd71a0b0c7715ec0f36bf0bf8c018d07e339d
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-11-17 12:07:41 +0100

    Remove JS files

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var nodeFirefox = require('node-firefox');
-
-nodeFirefox.startSimulator().then(function(simulator) {
-  nodeFirefox.connect(simulator.port).then(function(client) {
-    console.log(client);
-  });
-});
