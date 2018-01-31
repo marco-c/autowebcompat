c8b6e7a90590e5e27586e1fbcfd03ff710212688
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-02-01 01:28:15 +0530

    Add sequence number column and sort the output

--- data_inconsistencies.py
+++ data_inconsistencies.py
@@ -9,19 +9,30 @@ def get_inconsistencies():
     for file in files:
         parts = os.path.splitext(file)[0].split('_')
 
-        ID = parts[0]
+        ID = int(parts[0])
         if ID not in parsed:
             parsed[ID] = {}
         element = '_'.join(parts[1:-1])
-        if element not in parsed[ID]:
-            parsed[ID][element] = []
-        parsed[ID][element].append(parts[-1])
+        if element:
+            spl = element.split('_')
+            sequence = int(spl[-1])
+            element = '_'.join(spl[:-1])
+        else:
+            element = None
+            sequence = 0
+
+        if (element, sequence) not in parsed[ID]:
+            parsed[ID][(element, sequence)] = []
+        parsed[ID][(element, sequence)].append(parts[-1])
 
     incons = []
     for key, value in parsed.items():
-        for element, browsers in value.items():
+        for (element, sequence), browsers in value.items():
             if len(browsers) < 2:
-                incons.append([key, element, 'firefox' in browsers, 'chrome' in browsers])
+                incons.append([key, element, sequence, 'firefox' in browsers, 'chrome' in browsers])
+    
+    incons.sort(key=lambda x: x[2])
+    incons.sort(key=lambda x: x[0])
     return incons
 
 
@@ -33,7 +44,7 @@ def main():
     print('[*] Writing to inconsistencies.csv... ', end='')
     with open('inconsistencies.csv', 'w') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
-            writer.writerow(['WEBCOMPAT-ID', 'ELEMENT-ID', 'FIREFOX', 'CHROME'])
+            writer.writerow(['WEBCOMPAT-ID', 'ELEMENT-ID', 'SEQUENCE-NO', 'FIREFOX', 'CHROME'])
             for line in incons:
                 writer.writerow(line)
     print('Done!')
