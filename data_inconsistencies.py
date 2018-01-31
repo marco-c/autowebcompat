65659f7921f4f9b4e80384ccfc1cfaae1c1a5213
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-02-01 00:14:49 +0530

    Complete data_inconsistencies.py

--- data_inconsistencies.py
+++ data_inconsistencies.py
@@ -1,22 +1,44 @@
 import os
 import json
+import csv
 
 
 def get_inconsistencies():
     files = os.listdir('./data/')
 
-    parsed_files = []
+    parsed = {}
     for file in files:
         parts = os.path.splitext(file)[0].split('_')
-        info = {
-            'id': parts[0],
-            'browser': parts[-1],
-            'element': '_'.join(parts[1:-1])
-        }
-        parsed_files.append(info)
-    
-    print(json.dumps(parsed_files, indent=2))
+        
+        ID = parts[0]
+        if ID not in parsed:
+            parsed[ID] = {}
+        element = '_'.join(parts[1:-1])
+        if element not in parsed[ID]:
+            parsed[ID][element] = []
+        parsed[ID][element].append(parts[-1])
+
+    incons = []
+    for key, value in parsed.items():
+        for element, browsers in value.items():
+            if len(browsers) < 2:
+                incons.append([key, element, 'firefox' in browsers, 'chrome' in browsers])
+    return incons
 
 
+def main():
+    print('[*] Getting inconsistencies in screenshots...')
+    incons = get_inconsistencies()
+    print('[*] {} inconsistencies found.'.format(len(incons)))
 
-get_inconsistencies()
+    print('[*] Writing to inconsistencies.csv... ', end='')
+    with open('inconsistencies.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(['WEBCOMPAT-ID', 'ELEMENT-ID', 'FIREFOX', 'CHROME'])
+            for line in incons:
+                writer.writerow(line)
+    print('Done!')
+
+
+if __name__=='__main__':
+    main()
