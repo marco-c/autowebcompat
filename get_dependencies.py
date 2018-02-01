9f95693b459d54024c26d3bb784efd121245324d
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-02-01 18:31:03 +0530

    Add status messages and download progress bar in get_dependencies.py (#30)

--- get_dependencies.py
+++ get_dependencies.py
@@ -1,22 +1,41 @@
+import sys
 import os
-import shutil
 import tarfile
 from zipfile import ZipFile
 import requests
 
 
+def download(url, filename):
+    with open(filename, 'wb') as f:
+        response = requests.get(url, stream=True)
+        total = response.headers.get('content-length')
+
+        if total is None:
+            f.write(response.content)
+        else:
+            downloaded = 0
+            total = int(total)
+            for data in response.iter_content(chunk_size=max(int(total / 1000), 1024 * 1024)):
+                downloaded += len(data)
+                f.write(data)
+                done = int(50 * downloaded / total)
+                sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50 - done)))
+                sys.stdout.flush()
+    sys.stdout.write('\n')
+
+
+print('[*] Extracting webdriver archives...')
 for f in ['geckodriver', 'nightly', 'chromedriver', 'chrome-linux']:
     tar = tarfile.open('tools/%s.tar.xz' % f, 'r:xz')
     tar.extractall(path='tools/')
     tar.close()
 
-r = requests.get('https://www.dropbox.com/s/7f5uok2alxz9j1r/data.zip?dl=1', stream=True)
+print('[*] Downloading data.zip...')
+download('https://www.dropbox.com/s/7f5uok2alxz9j1r/data.zip?dl=1', 'data.zip')
 
-with open('data.zip', 'wb') as f:
-    r.raw.decode_content = True
-    shutil.copyfileobj(r.raw, f)
-
+print('[*] Extracting data.zip...')
 with ZipFile('data.zip', 'r') as z:
     z.extractall()
 
 os.remove('data.zip')
+print('[*] Completed!')
