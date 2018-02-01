87a9e174b2e431d22d619d910a86bc046aa61e05
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-02-01 18:10:30 +0530

    Make modifications according to review

--- get_dependencies.py
+++ get_dependencies.py
@@ -1,7 +1,27 @@
+import sys
+import requests
 import os
 import tarfile
 from zipfile import ZipFile
-from utils import download
+
+
+def download(url, filename):
+    with open(filename, 'wb') as f:
+            response = requests.get(url, stream=True)
+            total = response.headers.get('content-length')
+
+            if total is None:
+                f.write(response.content)
+            else:
+                downloaded = 0
+                total = int(total)
+                for data in response.iter_content(chunk_size=max(int(total / 1000), 1024 * 1024)):
+                    downloaded += len(data)
+                    f.write(data)
+                    done = int(50 * downloaded / total)
+                    sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50 - done)))
+                    sys.stdout.flush()
+    sys.stdout.write('\n')
 
 
 print('[*] Extracting webdriver archives...')
