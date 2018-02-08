d80a80bd6944ff4478b79acae85f86f5b4396311
Amit <amit2rockpower@gmail.com>
2018-02-08 13:02:34 +0530

    updating according to travis ci

--- collect.py
+++ collect.py
@@ -17,8 +17,8 @@ elif sys.platform.startswith("darwin"):    # MAC OS X
 
 elif sys.platform.startswith("win32"):
     path = os.getcwd()
-    chrome_bin = path+ '\\tools\\Google\\Chrome\\Application\\chrome.exe'
-    nightly_bin = path+ '\\tools\\Nightly\\firefox.exe'
+    chrome_bin = path + '\\tools\\Google\\Chrome\\Application\\chrome.exe'
+    nightly_bin = path + '\\tools\\Nightly\\firefox.exe'
 
 
 utils.mkdir('data')
