36a603da177becbaec2dcfefc3db7b6d907cfc5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-02-16 00:56:28 +0100

    Remove unneeded fallback urlparse import

--- pretrain.py
+++ pretrain.py
@@ -1,9 +1,6 @@
 from functools import lru_cache
 import random
-try:
-    from urllib.parse import urlparse
-except ImportError:
-    from urlparse import urlparse
+from urllib.parse import urlparse
 import itertools
 
 import network
