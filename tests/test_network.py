b6e94203f26282ba56afc2ad5d5ca66868b041b3
vibhu bhatia <vibhu2397@gmail.com>
2018-03-08 18:29:46 +0530

    fixed formatting errors

--- tests/test_network.py
+++ tests/test_network.py
@@ -9,29 +9,27 @@ arr2 = np.array([[0, 1]], dtype=np.float32)
 arr3 = np.array([[0, 0]], dtype=np.float32)
 arr4 = np.array([[3, 2]], dtype=np.float32)
 
+y_pred = np.array([0, 0.4, 0.7, 0.6, 0.5])
+y_true = np.array([0, 1, 0, 0, 1])
+
 
 def test_eucledian_distance():
     dist = network.euclidean_distance([arr1, arr2])
-    dist2 = network.euclidean_distance([arr3, arr4])
     assert (K.eval(dist) == 1)
-    assert (math.floor(K.eval(dist2) * 100) == 360)
+    dist2 = network.euclidean_distance([arr3, arr4])
+    assert(math.floor(K.eval(dist2) * 100) == 360)
 
 
 def test_eucl_distance_output_shape():
-    vect = [arr1.shape, arr2.shape]
-    assert (network.eucl_dist_output_shape(vect) == (1, 1))
+    assert(network.eucl_dist_output_shape([arr1.shape, arr2.shape]) == (1, 1))
+    assert(network.eucl_dist_output_shape([arr3.shape, arr4.shape]) == (1, 1))
 
 
 def test_contrastive_loss():
-    label = [0, 1]  # possible values for output
-    euclid_dist = network.euclidean_distance([arr1, arr2])  # it returns 1
-    loss1 = network.contrastive_loss(label[0], euclid_dist)
-    loss2 = network.contrastive_loss(label[1], euclid_dist)
-    assert (K.eval(loss1) == 0)  # since the output and the label are far away
-    assert (K.eval(loss2) == 1)  # since the output and the label are close
+    assert(K.eval(network.contrastive_loss(0, 1)) == 0)  # since the output and the label are far away
+    assert(K.eval(network.contrastive_loss(1, 1)) == 1)  # since the output and the label are close
+    assert(math.floor(K.eval(network.contrastive_loss(y_true, y_pred)) * 100) == 33)
 
 
 def test_accuracy():
-    y_pred = np.array([0, 0.4, 0.7, 0.6, 0.5])
-    y_true = np.array([0, 1, 0, 0, 1])
-    assert (math.floor(K.eval(network.accuracy(y_true, y_pred)) * 100) == 60)
+    assert(math.floor(K.eval(network.accuracy(y_true, y_pred)) * 100) == 60)
