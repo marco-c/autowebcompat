2b4b3401a4582627617d055b79d0b44483519052
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-03-17 21:30:52 +0000

    Merge allOptimizers and SUPPORTED_OPTIMIZERS

--- autowebcompat/network.py
+++ autowebcompat/network.py
@@ -4,7 +4,12 @@ from keras.models import Model
 from keras.optimizers import SGD, Adam, Nadam, RMSprop
 
 SUPPORTED_NETWORKS = ['inception', 'vgglike', 'vgg16']
-SUPPORTED_OPTIMIZERS = ['sgd', 'adam', 'nadam', 'rms']
+SUPPORTED_OPTIMIZERS = {
+    'sgd': SGD(lr=0.0003, decay=1e-6, momentum=0.9, nesterov=True),
+    'adam': Adam(),
+    'nadam': Nadam(),
+    'rms': RMSprop()
+}
 
 
 def euclidean_distance(vects):
@@ -160,13 +165,7 @@ def accuracy(y_true, y_pred):
 
 
 def compile(model, optimizer='sgd', loss_func=contrastive_loss):
-    allOptimizers = {
-        'sgd': SGD(lr=0.0003, decay=1e-6, momentum=0.9, nesterov=True),
-        'adam': Adam(),
-        'nadam': Nadam(),
-        'rms': RMSprop()
-    }
-    assert optimizer in allOptimizers, '%s is an invalid optimizer' % optimizer
-    opt = allOptimizers[optimizer]
+    assert optimizer in SUPPORTED_OPTIMIZERS, '%s is an invalid optimizer' % optimizer
+    opt = SUPPORTED_OPTIMIZERS[optimizer]
 
     model.compile(loss=loss_func, optimizer=opt, metrics=[accuracy])
