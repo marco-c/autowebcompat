dc3562a72b80d9582150c28a7d314a1b82a67971
PAWAN SASANKA AMMANAMANCHI <pass2pawan@gmail.com>
2018-03-18 16:03:30 +0530

    Travis errors
    Travis errors

--- tests/test_labels.py
+++ tests/test_labels.py
@@ -2,13 +2,13 @@ import csv
 import os
 
 
-def test_labels() :
-    with open('../labels.csv') as csvfile:
-        reader = csv.reader(csvfile,delimiter=',')
+def test_labels():
+    with open('..\labels.csv') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
-            if row[0] == 'Image Name' :
+            if row[0] == 'Image Name':
                 continue
-            file_name_firefox = '../data/'+ row[0] + '_' + 'firefox.png'
-            file_name_chrome = '../data/'+ row[0] + '_' + 'chrome.png'
+            file_name_firefox = '..\data\\' + row[0] + '_' + 'firefox.png'
+            file_name_chrome = '..\data\\' + row[0] + '_' + 'chrome.png'
             assert os.path.exists(file_name_firefox)
             assert os.path.exists(file_name_chrome)
