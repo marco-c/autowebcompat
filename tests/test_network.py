c56978fef6c820a8f23eaafb3d1a4998f247c4cf
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2018-03-20 17:31:07 +0600

    add flake8-quotes
    run flake8 with quotes
    added flake8-quotes

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,9 +1,8 @@
 import math
 
+from autowebcompat import network
 from keras import backend as K
 import numpy as np
-
-from autowebcompat import network
 
 arr1 = np.array([[1, 1]], dtype=np.float32)
 arr2 = np.array([[0, 1]], dtype=np.float32)
