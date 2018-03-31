47d5951bfae3acd027a7b7cdf2361ae522dec740
ANUJA NEGI <anujanegi1@gmail.com>
2018-03-31 23:46:50 +0530

    Update fix#68

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -48,7 +48,7 @@ def test_read_labels():
 
 
 def test_write_labels(tmpdir):
-    label = {1: 1, 2: 2}
+    label = {'1': '1', '2': '2'}
     file_path = tmpdir.join('test.csv')
     utils.write_labels(label, file_name=file_path)
     assert(os.path.exists(file_path))
