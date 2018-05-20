d058349d2c1e8780a7c9ee9256aba2a5bd3446dc
Parminder Singh <parmsingh129@gmail.com>
2018-05-20 21:30:10 +0530

    customise algo for purpose, load images in array

--- analyse_false.py
+++ analyse_false.py
@@ -1,82 +1,99 @@
-from functools import lru_cache
-from urllib.parse import urlparse
+import argparse
 from keras.models import load_model
 from autowebcompat.network import contrastive_loss
-import itertools
 import tkinter as tk
 from PIL import ImageTk, Image
 import numpy as np
 
 from autowebcompat import utils
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--classification_type', type=str, choices=utils.CLASSIFICATION_TYPES, default=utils.CLASSIFICATION_TYPES[0], help='Select the classification_type for training')
+
+args = parser.parse_args()
+
 bugs = utils.get_bugs()
 
-utils.prepare_images()
-all_images = utils.get_all_images()[:3000]  # 3000
-image = utils.load_image(all_images[0])
-input_shape = image.shape
 BATCH_SIZE = 32
 EPOCHS = 50
 
-bugs_to_website = {}
-for bug in bugs:
-    bugs_to_website[bug['id']] = urlparse(bug['url']).netloc
+labels = utils.read_labels()
 
+utils.prepare_images()
+all_image_names = [i for i in utils.get_images() if i in labels]
+print(all_image_names)
+all_images = sum([[i + '_firefox.png', i + '_chrome.png'] for i in all_image_names], [])
+image = utils.load_image(all_images[0])
+input_shape = image.shape
 
-@lru_cache(maxsize=len(all_images))
-def site_for_image(image):
-    bug = image[:image.index('_')]
-    return bugs_to_website[int(bug)]
-
-
-def are_same_site(image1, image2):
-    return site_for_image(image1) == site_for_image(image2)
+def load_pair(fname):
+    return [fname + '_firefox.png', fname + '_chrome.png']
 
 def couples_generator(images):
-    for image_couple in itertools.combinations(images, 2):
-        yield image_couple, 1 if are_same_site(image_couple[0], image_couple[1]) else 0
-
+    for i in images:
+        yield load_pair(i), utils.to_categorical_label(labels[i], args.classification_type)
 
 def gen_func(images):
-    return utils.balance(couples_generator(images))
+    return couples_generator(images)
 
-
-all_couples_len = sum(1 for e in gen_func(all_images))
+number_of_couples = sum(1 for e in gen_func(all_image_names))
 
 print(input_shape)
 
 data_gen = utils.get_ImageDataGenerator(all_images, input_shape)
-all_iterator = utils.CouplesIterator(utils.make_infinite(gen_func, all_images), input_shape, data_gen, BATCH_SIZE)
+all_iterator = utils.CouplesIterator(gen_func(all_image_names), input_shape, data_gen, BATCH_SIZE)
 
-model = load_model('pretrain.h5', custom_objects={'contrastive_loss': contrastive_loss})
+model = load_model('best_train_model.hdf5', custom_objects={'contrastive_loss': contrastive_loss})
 
-predictions = model.predict_generator(all_iterator, steps=all_couples_len / BATCH_SIZE)
+images_for_analysis = []
+for x_batch, y_batch in all_iterator:
+    predictions = model.predict(x_batch)
 
-rounded_preds = np.round(predictions)
+    for truth, prediction, image in zip(y_batch, predictions, x_batch):
+        if truth != math.round(prediction):
+            images_for_analysis.append([*x_batch, prediction])
 
-new_gen = utils.make_infinite(gen_func, all_images)
-false_vals = []
-for prediction in predictions:
-    images, classid = next(new_gen)
-    if classid != prediction:
-        false_vals.append(images)
 
+# Create UI
 window = tk.Tk()
 
-panel1 = tk.Label(window)
-panel1.pack(side="left", padx=10)
-panel2 = tk.Label(window)
-panel2.pack(side="left", padx=10)
+image_view = tk.Frame(window)
+image_view.pack()
 
-def show_next_image(image1, image2):
-    img1 = ImageTk.PhotoImage(Image.open(image1))
+panel1 = tk.Label(image_view)
+panel1.grid(row=0, column=0)
+
+panel1_text = tk.Label(image_view)
+panel1_text.grid(row=1, column=0)
+
+panel2 = tk.Label(image_view)
+panel2.grid(row=0, column=1)
+
+panel2_text = tk.Label(image_view)
+panel2_text.grid(row=1, column=1)
+
+prediction_text = tk.Label(image_view)
+prediction_text.grid(row=2)
+
+# Fill with image
+def show_next_image():
+    global panel1, panel2, panel1_text, panel2_text
+
+    img1 = ImageTk.PhotoImage(Image.open("data/" + image1))
     panel1.configure(image=img1)
     panel1.image = img1
+    panel1_text.config(text=image1)
 
-    img2 = ImageTk.PhotoImage(Image.open(image2))
+    img2 = ImageTk.PhotoImage(Image.open("data/" + image2))
     panel2.configure(image=img2)
     panel2.image = img2
+    panel2_text.config(text=image2)
 
+    prediction_text.config("Prediction: " + str(prediction))
 
+show_next_image(*false_vals[0])
+
+next = tk.Button(window, text="Next")
+next.pack(side="bottom", ipadx=30, ipady=30, pady=20)
 
 window.mainloop()
