b43b529a3c98d7fd7e21261d2379b1c88c872c00
sagarvijaygupta <sagarguptasml@gmail.com>
2018-06-16 11:11:01 +0530

    Fix grammatical error in help of argument for weights.

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ random.seed(42)
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--network', type=str, choices=network.SUPPORTED_NETWORKS, help='Select the network to use for training')
 parser.add_argument('-o', '--optimizer', type=str, choices=network.SUPPORTED_OPTIMIZERS, help='Select the optimizer to use for training')
-parser.add_argument('-w', '--weights', type=str, choices=network.SUPPORTED_WEIGHTS, help='Select the weights to be loaded for given model')
+parser.add_argument('-w', '--weights', type=str, choices=network.SUPPORTED_WEIGHTS, help='Select the weights to be loaded for the given model')
 parser.add_argument('-ct', '--classification_type', type=str, choices=utils.CLASSIFICATION_TYPES, default=utils.CLASSIFICATION_TYPES[0], help='Select the classification_type for training')
 parser.add_argument('-es', '--early_stopping', dest='early_stopping', action='store_true', help='Stop training training when validation accuracy has stopped improving.')
 
