e3ad59dffb966a5f08a58bdbbe4ad5c989888f02
shanesims <shane.sims.ss@gmail.com>
2018-06-18 16:57:03 -0700

    changed def of load_images in utils.py, ref issue #191, to run vgg19 on colab

--- autowebcompat/utils.py
+++ autowebcompat/utils.py
@@ -80,18 +80,23 @@ def prepare_images():
 images = {}
 
 
-def load_image(fname, parent_dir='data_resized'):
-    global images
+ def load_image(fname, parent_dir='data_resized'):
+     img = load_img(os.path.join(parent_dir, fname), target_size=(224, 224))
+     x = img_to_array(img, data_format=keras.backend.image_data_format()
+     return x
 
-    if fname in images:
-        return images[fname]
-
-    img = load_img(os.path.join(parent_dir, fname), target_size=(224, 224))
-    x = img_to_array(img, data_format=keras.backend.image_data_format())
-
-    images[fname] = x
-
-    return x
+#def load_image(fname, parent_dir='data_resized'):
+#    global images
+#
+#    if fname in images:
+#        return images[fname]
+#
+#    img = load_img(os.path.join(parent_dir, fname), target_size=(224, 224))
+#    x = img_to_array(img, data_format=keras.backend.image_data_format())
+#
+#    images[fname] = x
+#
+#    return x
 
 
 def get_ImageDataGenerator(images, image_shape, parent_dir='data_resized'):
