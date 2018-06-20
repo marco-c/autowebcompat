277f3705e9eea2a7e95423fb99d4d58e0ceed8d4
shanesims <shane.sims.ss@gmail.com>
2018-06-20 11:37:20 -0700

    removing Colab test code

--- train.py
+++ train.py
@@ -94,14 +94,6 @@ train_history = model.fit_generator(train_iterator, callbacks=callbacks_list, va
 score = model.evaluate_generator(test_iterator, steps=test_couples_len / BATCH_SIZE)
 print(score)
 
-Y_pred = model.predict_generator(validation_generator, TEST_SAMPLE // BATCH_SIZE+1)
-y_pred = np.argmax(Y_pred, axis=1)
-print('Confusion Matrix')
-print(confusion_matrix(('Y','D+N'), y_pred))
-print('Classification Report')
-target_names = ['Y', 'D+N']
-print(classification_report(('Y','D+N'), y_pred, target_names=target_names))
-
 train_history = train_history.history
 train_history.update({'epoch time': timer.epoch_times})
 information = vars(args)
